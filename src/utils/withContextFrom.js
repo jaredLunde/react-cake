2d64451de7fb3cc2d7d6b8bcc9dbb617d9221bcf
Jared Lunde <jared.lunde@gmail.com>
2017-11-24 02:31:46 -0700

    Improved withContextFrom

--- src/utils/withContextFrom.js
+++ src/utils/withContextFrom.js
@@ -3,15 +3,14 @@ import {wrapDisplayName} from '../helpers'
 import cloneIfElement from './cloneIfElement'
 
 
-export default Component => class withContextFrom extends React.PureComponent {
-  static displayName = wrapDisplayName('withContextFrom', Component)
-  static contextTypes = Component.childContextTypes
-
-  render () {
-    let {children, ...props} = this.props
-    const context = this.context || {}
-    props = {...context, ...props}
-    
-    return cloneIfElement(children, props)
-  }
+export default Component => {
+  const withContextFrom = ({children, ...props}, context) => cloneIfElement(
+    children,
+    {
+      ...props,
+      ...context
+    }
+  )
+  withContextFrom.contextTypes = Component.childContextTypes
+  return withContextFrom
 }
