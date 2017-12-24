1e44e2cfba6ffee009903b03b5d2af6e4dfb7645
Jared Lunde <jared.lunde@gmail.com>
2017-12-23 20:06:39 -0700

    tbd

--- src/utils/createOptimized.js
+++ src/utils/createOptimized.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-
+/**
 export default function (Component, props, children) {
   const typeOfComponent = typeof Component
   const prototype = Component && Component.prototype
@@ -11,13 +11,42 @@ export default function (Component, props, children) {
   }
 
   return (
-       typeOfComponent === 'function'
-    && (!prototype || !prototype.isReactComponent)
-  )
-    ? Component(props)
-    : typeOfComponent === 'number' || (isString && props === void 0)
-      ? Component
-      : isString || (prototype && prototype.isReactComponent)
+    (typeOfComponent === 'function' && (!prototype || !prototype.isReactComponent))
+      ? Component(props)
+      : isString || typeOfComponent === 'number' || (prototype && prototype.isReactComponent)
         ? React.createElement(Component, props)
         : React.cloneElement(Component, props)
+  )
+}
+*/
+
+export default function (Component, props, children) {
+  const typeOfComponent = typeof Component
+  const prototype = Component && Component.prototype
+  const isReactComponent = prototype && prototype.isReactComponent
+
+  if (typeOfComponent === 'function' && !isReactComponent) {
+    if (children !== void 0) {
+      props.children = children
+    }
+
+    return Component(props)
+  }
+
+  const shouldCreateElement = isReactComponent || typeOfComponent === 'string' || typeOfComponent === 'number'
+
+  if (children !== void 0) {
+    return (
+      shouldCreateElement
+      ? React.createElement(Component, props, children)
+      : React.cloneElement(Component, props, children)
+    )
+  } else {
+    return (
+      shouldCreateElement
+      ? React.createElement(Component, props)
+      : React.cloneElement(Component, props)
+    )
+  }
+
 }
